42a147cac1afa22aa148c99ac6f18f62a7964d61
yweber <yannick.weber@hivemq.com>
2020-06-10 10:21:12 +0200

    dc-square -> HiveMQ

--- src/main/resources/archetype-resources/src/main/java/HelloWorldMain.java
+++ src/main/resources/archetype-resources/src/main/java/HelloWorldMain.java
@@ -3,7 +3,7 @@
 #set( $symbol_escape = '\' )
 
 /*
- * Copyright 2018 dc-square GmbH
+ * Copyright 2020 HiveMQ GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
